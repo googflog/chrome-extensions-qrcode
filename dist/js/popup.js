ab9b58cba432528c113db7e8652401423dd70df5
t-tsujimoto-futurek <googflog@gmail.com>
2025-02-21 18:11:40 +0900

    modify ビルドツールやめた

--- dist/js/popup.js
+++ dist/js/popup.js
@@ -1,2 +1,222 @@
-$((function(){var t=function(){let t={};if(1<document.location.search.length){let e=document.location.search.substring(1).split("&");for(let o=0,n=e.length;o<n;o++){let n=e[o].split("="),i=decodeURIComponent(n[0]),r=decodeURIComponent(n[1]);t[i]=decodeURIComponent(r)}}return t}();$(".version").append(chrome.runtime.getManifest().version);const e=$("textarea"),o=$(".contets"),n=$("body");if(t.url){const i=t.url,a=t.title;r(i),e.val(i),s(i,a),o.addClass("expansion"),n.addClass("context_mode")}else if(chrome.tabs)chrome.tabs.query({active:!0,currentWindow:!0},(function(t){const o=decodeURIComponent(t[0].url),n=t[0].title;r(o),e.val(o),s(o,n)}));else{const t=decodeURIComponent(window.location.href);$("#qr").qrcode({width:1e3,height:1e3,text:t}),e.val(t)}var i;function r(t){$("#qr").html(""),$("#qr").qrcode({width:1e3,height:1e3,text:t});const e=t.match(/^https?:\/{2,}(.*?)(?:\/|\?|#|$)/)[1].replace(/\./g,"_"),o=(n=new Date,i=n.getFullYear().toString(),r=(n.getMonth()+1).toString(),a=n.getDate().toString(),s=n.getHours().toString(),c=n.getMinutes().toString(),l=n.getSeconds().toString(),i+r+a+s+c+l);var n,i,r,a,s,c,l;const d=$("canvas")[0].toDataURL();$(".download").attr("href",d),$(".download").attr("download","qr_"+o+"_"+e+".png")}function a(){var t=localStorage.getItem("qrcodeextensions12345");if(t){var e=JSON.parse(t);return console.log(e),e}return!1}function s(t,e){var o=a(),n=[];console.log("historyListObj",o);var i=!1;if(o)for(var r in o)n.push({url:o[r].url,title:o[r].title,date:o[r].date}),t==o[r].url&&(i=!0);i||n.push({url:t,title:e,date:new Date}),30<n.length&&n.shift();var s=JSON.stringify(n);localStorage.setItem("qrcodeextensions12345",s)}$(document).on("click","#qr",(function(){$(".contets").hasClass("expansion")?$(".contets").removeClass("expansion"):$(".contets").addClass("expansion")})),$("textarea").on("keydown",(function(t){clearTimeout(i),i=setTimeout((function(){var t=$("textarea").val().toString();t.match(/[\u30a0-\u30ff\u3040-\u309f\u3005-\u3006\u30e0-\u9fcf]/)?(console.log("JP",t),r(t=Encoding.convert(t,"SJIS"))):(console.log("EN",t),r(encodeURI(t)))}),300)})),$("textarea").on("click",(function(t){$(this).addClass("click-open")})),$(".history").on("click",(function(){$(".contets_inline").addClass("history"),$(".history-list").show(),$(".back").show(),$(".history-clear").show(),$(".option").show(),function(){var t=a();if(t.reverse(),t)for(var e in $(".history-list ul").html(""),t)$(".history-list ul").append('<li data-obj="'+t[e].url+'"><dl><dt>'+t[e].title+"</dt><dd>"+t[e].url+"</dd></dl></li>")}()})),$(".back").on("click",(function(){$(".contets_inline").removeClass("history")})),$(document).on("click",".history-list li",(function(){var t=$(this).data("obj");$("textarea").val(t),r(encodeURI($("textarea").val())),$(".contets_inline").removeClass("history")})),$(".option").on("click",(function(){chrome.runtime.openOptionsPage?chrome.runtime.openOptionsPage():window.open(chrome.runtime.getURL("options.html"))})),new PerfectScrollbar(".history-list")}));
-//# sourceMappingURL=popup.js.map
+$(function () {
+  var url_val;
+
+  var from_context = getQueryString();
+
+  $(".version").append(chrome.runtime.getManifest().version);
+
+  //ロード時QR表示
+
+  const $textarea = $(".url-textarea");
+  const $contets = $(".contets");
+  const $body = $("body");
+  if (from_context.url) {
+    // contextから
+    const url = from_context.url;
+    const title = from_context.title;
+    drawQr(url);
+    $textarea.val(url);
+    historySave(url, title);
+    $contets.addClass("is-expansion");
+    $body.addClass("context_mode");
+  } else if (chrome.tabs) {
+    // popから
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const url = decodeURIComponent(tabs[0].url);
+      const title = tabs[0].title;
+      drawQr(url);
+      $textarea.val(url);
+      historySave(url, title);
+    });
+  } else {
+    // historyから
+    const url = decodeURIComponent(window.location.href);
+    $("#qrcode").qrcode({ width: 1000, height: 1000, text: url });
+    // $("#qrcode").qrcode(url);
+    $textarea.val(url);
+  }
+
+  //QRコードを拡大
+  $(document).on("click", "#qrcode", function () {
+    if (!$(".contets").hasClass("is-expansion")) {
+      $(".contets").addClass("is-expansion");
+    } else {
+      $(".contets").removeClass("is-expansion");
+    }
+  });
+
+  //キーボードでURLを修正した
+  var timeoutid;
+  $(".url-textarea").on("keydown", function (e) {
+    clearTimeout(timeoutid);
+    timeoutid = setTimeout(function () {
+      var str = $(".url-textarea").val().toString();
+      if (str.match(/[\u30a0-\u30ff\u3040-\u309f\u3005-\u3006\u30e0-\u9fcf]/)) {
+        console.log("JP", str);
+        str = Encoding.convert(str, "SJIS");
+        drawQr(str);
+      } else {
+        console.log("EN", str);
+        drawQr(encodeURI(str));
+      }
+    }, 300);
+  });
+
+  $(".url-textarea").on("click", function (e) {
+    $(this).addClass("click-open");
+  });
+
+  // 履歴表示ボタン
+  $(".button[data-type='history']").on("click", function () {
+    $(".contets-inline").addClass("is-history");
+    $(".history-list").show();
+    $(".back-button").show();
+    $(".history-clear").show();
+    $(".option").show();
+    historyListShow();
+  });
+
+  // 戻るボタン
+  $(".button[data-type='back']").on("click", function () {
+    $(".contets-inline").removeClass("is-history");
+  });
+
+  // 履歴リスト
+  $(document).on("click", ".history-list li", function () {
+    var url_val = $(this).data("obj");
+    $(".url-textarea").val(url_val);
+    drawQr(encodeURI($(".url-textarea").val()));
+    $(".contets-inline").removeClass("is-history");
+  });
+
+  // オプション表示ボタン
+  $(".option").on("click", function () {
+    if (chrome.runtime.openOptionsPage) {
+      chrome.runtime.openOptionsPage();
+    } else {
+      window.open(chrome.runtime.getURL("options.html"));
+    }
+  });
+
+  new PerfectScrollbar(".history-list");
+
+  /**
+   * QRを描画
+   * @param {string} val
+   */
+  function drawQr(val) {
+    $("#qrcode").html("");
+    $("#qrcode").qrcode({ width: 1000, height: 1000, text: val });
+
+    //ドメインだけ取り出し
+    const urlToValidFilename = val.match(/^https?:\/{2,}(.*?)(?:\/|\?|#|$)/)[1].replace(/\./g, "_");
+
+    //画像ダウンロード用に変換
+    const time = getDate();
+    //画像データを取得
+    const imgData = $("canvas")[0].toDataURL();
+
+    $(".button[data-type='download']").attr("href", imgData);
+    $(".button[data-type='download']").attr("download", "qr_" + time + "_" + urlToValidFilename + ".png");
+  }
+
+  /**
+   * 日付取得
+   */
+  function getDate() {
+    var dt = new Date();
+    var year = dt.getFullYear().toString();
+    var month = (dt.getMonth() + 1).toString();
+    var date = dt.getDate().toString();
+    var hours = dt.getHours().toString();
+    var minutes = dt.getMinutes().toString();
+    var seconds = dt.getSeconds().toString();
+    return year + month + date + hours + minutes + seconds;
+  }
+
+  /**
+   * 履歴を表示
+   */
+  function historyListShow() {
+    var historyListObj = histryLoad();
+    historyListObj.reverse();
+    if (historyListObj) {
+      $(".history-list ul").html("");
+      for (var item in historyListObj) {
+        $(".history-list ul").append('<li data-obj="' + historyListObj[item].url + '"><dl><dt>' + historyListObj[item].title + "</dt><dd>" + historyListObj[item].url + "</dd></dl></li>");
+      }
+    }
+  }
+
+  /**
+   * ローカルストレージから履歴をロードする
+   * @return {object} 履歴の配列
+   */
+  function histryLoad() {
+    var getjson = localStorage.getItem("qrcodeextensions12345");
+    if (getjson) {
+      var historyListObj = JSON.parse(getjson);
+      console.log(historyListObj);
+      return historyListObj;
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * ローカルストレージに履歴を保存する
+   * @param {string} url
+   * @param {string} title
+   */
+  function historySave(url, title) {
+    var historyListObj = histryLoad();
+    var array = [];
+    console.log("historyListObj", historyListObj);
+    var sameURL = false;
+    if (historyListObj) {
+      for (var item in historyListObj) {
+        array.push({
+          url: historyListObj[item].url,
+          title: historyListObj[item].title,
+          date: historyListObj[item].date,
+        });
+        if (url == historyListObj[item].url) {
+          sameURL = true;
+        }
+      }
+    }
+    if (!sameURL) {
+      array.push({
+        url: url,
+        title: title,
+        date: new Date(),
+      });
+    }
+    if (30 < array.length) {
+      array.shift();
+    }
+    var setjson = JSON.stringify(array);
+    localStorage.setItem("qrcodeextensions12345", setjson);
+  }
+
+  /**
+   * クエリストリング（URLパラメータ）をパースして返す
+   * @returns {Object} `{name: value, ...}`にパースする
+   */
+  function getQueryString() {
+    let result = {};
+
+    if (1 < document.location.search.length) {
+      let query = document.location.search.substring(1);
+      let parameters = query.split("&");
+
+      for (let i = 0, len = parameters.length; i < len; i++) {
+        let element = parameters[i].split("=");
+        let paramName = decodeURIComponent(element[0]);
+        let paramValue = decodeURIComponent(element[1]);
+        result[paramName] = decodeURIComponent(paramValue);
+      }
+    }
+    return result;
+  }
+});
